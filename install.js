d8401dbd70da3bac835a2db39e49fcbbea938126
zhangtao07 <zhangtao07@baidu.com>
2015-08-26 19:21:07 +0800

    fix bug for mac install

--- install.js
+++ install.js
@@ -8,13 +8,13 @@ var cp = require('child_process');
 //mac下安装http-sync模块发起同步请求
 //默认的http-sync-win只在windows下有效
 if(isMac){
-    install("http-sync","0.0.4");
+    install("http-sync","0.1.2");
 }
 
 
 function install(packageName,version){ 
-    var cmd = "npm install " + packageName + "@" + version;
-    console.log("install "+ packageName + " on mac. please wait.... ");
+    var cmd = "sudo npm install " + packageName + "@" + version;
+    console.log("sudo install "+ packageName + " on mac. please wait.... ");
     cp.exec(cmd ,function(err, stdout, stderr){
         console.log(stdout);
         if(err){        
